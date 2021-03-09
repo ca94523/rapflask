92c62dc43a4c854aaf817dbf5b067ed0b8d1f42a
akj_ubuntu <akjanto@gmail.com>
2021-03-09 13:15:51 +0700

    dd

--- r_scripts/install_r.sh
+++ r_scripts/install_r.sh
@@ -1,37 +1,22 @@
 #!/bin/bash
 set -e
-apt-get update
+#refer to https://docs.rstudio.com/resources/install-r/
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+export R_VERSION=4.0.4
+curl -O https://cdn.rstudio.com/r/centos-8/pkgs/R-${R_VERSION}-1-1.x86_64.rpm
+yum install -y R-${R_VERSION}-1-1.x86_64.rpm
+/opt/R/${R_VERSION}/bin/R --version
+ln -s /opt/R/${R_VERSION}/bin/R /usr/local/bin/R
+ln -s /opt/R/${R_VERSION}/bin/Rscript /usr/local/bin/Rscript
 
-apt-get -y install --no-install-recommends \
-      ca-certificates \
-      less \
-      libopenblas-base \
-      locales \
-      vim-tiny \
-      wget \
-      dirmngr \
-      gpg \
-      gpg-agent \
-      software-properties-common \
-      apt-transport-https
+R_HOME=${R_HOME:-/usr/local/bin/R}
 
-#apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
-apt-key adv --keyserver keyserver.ubuntu.com --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
-add-apt-repository 'deb http://cloud.r-project.org/bin/linux/debian buster-cran40/'
-apt-get update && apt-get -y install --no-install-recommends r-base r-base-dev
-rm -rf /var/lib/apt/lists/*
-
-#default install to /usr/lib/R
-R_HOME=${R_HOME:-/usr/lib/R}
-
-
-echo "options(repos = c(CRAN = 'https://packagemanager.rstudio.com/all/__linux__/bionic/latest'), download.file.method = 'libcurl')" >> /usr/lib/R/etc/Rprofile.site
-echo "options(HTTPUserAgent = sprintf('R/%s R (%s)', getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))" >> /usr/lib/R/etc/Rprofile.site
-Rscript -e "install.packages(c('littler', 'docopt'), repos='https://packagemanager.rstudio.com/all/__linux__/bionic/latest')"
+Rscript -e "install.packages(c('littler', 'docopt'), repos='https://packagemanager.rstudio.com/all/latest')"
+#saved to /opt/R/4.0.4/lib/R/library
 
 ## By default R_LIBS_SITE is unset, and defaults to this, so this is where `littler` will be.
 ## We set it here for symlinks, but don't make the env var persist (since it's already the default)
-R_LIBS_SITE=/usr/local/lib/R/site-library
+R_LIBS_SITE=/opt/R/4.0.4/lib/R/library
 ln -s ${R_LIBS_SITE}/littler/examples/install.r /usr/local/bin/install.r
 ln -s ${R_LIBS_SITE}/littler/examples/install2.r /usr/local/bin/install2.r
 ln -s ${R_LIBS_SITE}/littler/examples/installGithub.r /usr/local/bin/installGithub.r
--- r_scripts/install_rpackage.sh
+++ r_scripts/install_rpackage.sh
@@ -3,25 +3,26 @@
 set -e
 
 #remove/add packages as necessary
-apt-get update -qq && apt-get -y --no-install-recommends install \
-    libxml2-dev \
-    libcairo2-dev \
-    libgit2-dev \
-    default-libmysqlclient-dev \
-    libpq-dev \
-    libsasl2-dev \
-    libsqlite3-dev \
-    libssh2-1-dev \
-    zlib1g-dev \
-    make \
-    unixodbc-dev && \
-  rm -rf /var/lib/apt/lists/*
+#apt-get update -qq && apt-get -y --no-install-recommends install \
+#    libxml2-dev \
+#    libcairo2-dev \
+#    libgit2-dev \
+#    default-libmysqlclient-dev \
+#    libpq-dev \
+#    libsasl2-dev \
+#    libsqlite3-dev \
+#    libssh2-1-dev \
+#    zlib1g-dev \
+#    make \
+#    unixodbc-dev && \
+#  rm -rf /var/lib/apt/lists/*
 
 
 #remove/add packages as necessary
 #check here to check if binary, or source compiled, and check dependency
 #https://packagemanager.rstudio.com/client/#/repos/1/packages
-install2.r --error --skipinstalled -r https://packagemanager.rstudio.com/all/__linux__/bionic/latest -n 1 \
+R_LIBS_SITE=/opt/R/4.0.4/lib/R/library
+install2.r -l ${R_LIBS_SITE} --error --skipinstalled -r https://packagemanager.rstudio.com/all/latest -n 1 \
   dplyr \
   jsonlite \
   lubridate \
@@ -30,6 +31,6 @@ install2.r --error --skipinstalled -r https://packagemanager.rstudio.com/all/__l
   xgboost \
   zoo \
   stringr \
-  data.table \
+  data.table
 
- rm -rf /tmp/downloaded_packages
+# rm -rf /tmp/downloaded_packages
